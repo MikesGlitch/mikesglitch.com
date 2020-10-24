49140b7139878482ce591bafc5949d2bea2250bf
Michael Clark <mclarkgb@gmail.com>
2020-10-24 12:16:41 +0100

    test page work

--- blog/pages/index.vue
+++ blog/pages/index.vue
@@ -6,22 +6,9 @@
         blog
       </h1>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <nuxt-link to="/test">
+          Test page
+        </nuxt-link>
       </div>
     </div>
   </div>
