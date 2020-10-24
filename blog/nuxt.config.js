dce1c8d4ccbe3932b5814d868afcc36b696840d4
Michael Clark <mclarkgb@gmail.com>
2020-10-24 12:08:57 +0100

    test 2

--- blog/nuxt.config.js
+++ blog/nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  router: {
+    base: '/coderevver.github.io/'
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'blog',
