d71029e769d4c983214b25b247c0780230140b18
Michael Clark <mclarkgb@gmail.com>
2021-02-09 18:26:26 +0000

    removing comments

--- components/nav/Nav.vue
+++ components/nav/Nav.vue
@@ -68,14 +68,12 @@ export default {
 
     .nav__menu-toggle{
       display: flex;
-      // padding: 1rem;
 
       svg {
         width: 1.5rem;
         height: 1.5rem;
       }
 
-//  inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white
       @include tablet {
         display: none;
       }
