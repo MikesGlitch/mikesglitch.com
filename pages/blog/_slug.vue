90cb229307572bde3804ee3ee3194274100ba3c8
Michael Clark <mclarkgb@gmail.com>
2021-03-23 19:23:10 +0000

    sidebar

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,23 +1,31 @@
 <template>
-  <article>
-    <info-box :time-to-read="article.timeToRead">
-      <template #info-box>
-      <!-- This is a vue component inside markdown using slots -->
-      </template>
-    </info-box>
+  <div class="blog-page">
     <h1>{{ article.title }}</h1>
-    <p>{{ article.description }}</p>
-    <!--
-    <ul>
-      <li v-for="link of article.toc" :key="link.id">
-        <NuxtLink :to="`#${link.id}`">
-          {{ link.text }}
-        </NuxtLink>
-      </li>
-    </ul> -->
-    <nuxt-content :document="article" />
-    <p>Post last updated: {{ article.updatedAt }}</p>
-  </article>
+    <div class="blog-post">
+      <div class="blog-post__content">
+        <article>
+          <info-box :time-to-read="article.timeToRead">
+            <template #info-box>
+              <!-- This is a vue component inside markdown using slots -->
+            </template>
+          </info-box>
+          <!-- <h1>{{ article.title }}</h1> -->
+          <p>{{ article.description }}</p>
+          <!--
+        <ul>
+          <li v-for="link of article.toc" :key="link.id">
+            <NuxtLink :to="`#${link.id}`">
+              {{ link.text }}
+            </NuxtLink>
+          </li>
+        </ul> -->
+          <nuxt-content :document="article" />
+          <p>Post last updated: {{ article.updatedAt }}</p>
+        </article>
+      </div>
+      <BlogSidebar />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -30,16 +38,50 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@use 'assets/css/screen-breakpoints';
+.blog-page {
+  .blog-post {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: auto;
+
+    @include screen-breakpoints.desktop {
+      grid-template-columns: minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr);
+    }
+
+    .blog-sidebar {
+      grid-row: 1;
+
+      @include screen-breakpoints.tablet {
+        grid-column: 5;
+        grid-row: unset;
+      }
+    }
+
+    &__content {
+      grid-row: 2;
+      grid-gap: 10px;
+
+      @include screen-breakpoints.tablet {
+        grid-row: unset;
+        grid-column: 1/span 4;
+      }
+    }
+  }
+
   .nuxt-content h2 {
     font-weight: bold;
     font-size: 28px;
   }
+
   .nuxt-content h3 {
     font-weight: bold;
     font-size: 22px;
   }
+
   .nuxt-content p {
     margin-bottom: 20px;
   }
+}
 </style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="blog-list-page">
     <h1>Blog Posts</h1>
     <div class="blog-container">
       <div class="blog-posts">
@@ -14,36 +14,7 @@
           </NuxtLink>
         </div>
       </div>
-      <div class="blog-sidebar">
-        <div class="about-me-summary">
-          <img class="about-me-summary__image" src="/avatar.png" alt="Picture of me">
-          <h4 class="about-me-summary__title">About me</h4>
-          <p class="about-me-summary__description">Small paragraph about who I am and what I'm using the blog for</p>
-        </div>
-        <div class="search-site">
-          <h4 class="search-site__title">Search site</h4>
-          <input type="search" />
-          <button>Search</button>
-        </div>
-        <div class="tags-filters">
-          <h4 class="tags-filters__title">Tags</h4>
-          <div class="tags-filters__tags">
-            <a class="tag active" href="javascript:void(0)">All</a>
-            <a class="tag" href="javascript:void(0)">Old stuff</a>
-          </div>
-        </div>
-        <div class="category-filters">
-          <h4 class="category__title">Categories - might delete</h4>
-          <div class="category__categories">
-            <a class="" href="javascript:void(0)">Web development</a>
-            <a class="" href="javascript:void(0)">Flutter Desktop</a>
-            <a class="" href="javascript:void(0)">Old stuff</a>
-          </div>
-        </div>
-        <div class="rss-feed">
-          <h4 class="rss-feed__title">RSS Feed</h4>
-        </div>
-      </div>
+      <BlogSidebar />
     </div>
   </div>
 </template>
@@ -73,102 +44,44 @@ export default {
 <style lang="scss" scoped>
 @use 'assets/css/screen-breakpoints';
 
-.about-me-summary {
-  display: flex;
-  flex-direction: column;
-
-  &__image {
-    margin: 0 auto;
-    vertical-align: middle;
-    width: 150px;
-    height: 150px;
-    border-radius: 50%;
-    box-shadow: 0 8px 16px 0 rgb(0 0 0 / 20%);
-  }
-
-  &__title {
-    margin: 0;
-  }
-
-  &__description {
-    margin: 0;
-  }
-}
-
-.search-site {
-  margin-top: 1rem;
-}
-
-.category-filters {
-  margin-top: 1rem;
-}
-
-.tags-filters {
-  margin-top: 1rem;
-
-  &__tags{
-    display: flex;
-    flex-wrap: wrap;
-
-    .tag {
-      margin-right: .5rem;
-      margin-bottom: .5rem;
-      background-color: white;
-      border-radius: 15px;
-      padding: 0.5rem 1rem;
-      box-shadow: 0 8px 16px 0 rgb(0 0 0 / 20%);
-      font-weight: bold;
-
-      &:hover {
-        background-color: hotpink;
-        color: white;
-      }
-
-      &.active {
-        background-color: hotpink;
-        color: white;
-      }
-    }
-  }
-}
-
-.blog-container {
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: auto;
-
-  @include screen-breakpoints.desktop {
-    grid-template-columns: minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr);
-  }
-
-  .blog-sidebar {
-    padding: 0 1rem;
-    grid-row: 1;
-
-    @include screen-breakpoints.tablet {
-      grid-column: 5;
-      grid-row: unset;
-    }
-  }
-
-  .blog-posts {
+.blog-list-page {
+  .blog-container {
     display: grid;
     grid-gap: 10px;
     grid-template-columns: auto;
-    grid-row: 2;
-
-    img {
-      width: 100%;
-    }
-
-    @include screen-breakpoints.tablet {
-      grid-column: 1/span 4;
-      grid-template-columns: auto auto;
-      grid-row: unset;
-    }
 
     @include screen-breakpoints.desktop {
-      grid-template-columns: auto auto auto;
+      grid-template-columns: minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr);
+    }
+
+    .blog-sidebar {
+      grid-row: 1;
+
+      @include screen-breakpoints.tablet {
+        grid-column: 5;
+        grid-row: unset;
+      }
+    }
+
+    .blog-posts {
+      display: grid;
+      grid-row: 2;
+      grid-gap: 10px;
+      grid-template-columns: auto;
+
+      @include screen-breakpoints.tablet {
+        grid-row: unset;
+        grid-column: 1/span 4;
+        grid-template-columns: auto auto;
+      }
+
+      @include screen-breakpoints.desktop {
+        grid-template-columns: auto auto auto;
+      }
+
+      img {
+        width: 100%;
+      }
     }
   }
 }
