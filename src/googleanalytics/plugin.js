0dca87f954f3ce2191e268865679aeb57ca5ded7
Michael Clark <mclarkgb@gmail.com>
2021-03-26 21:19:45 +0000

    analytics screenview

--- src/googleanalytics/plugin.js
+++ src/googleanalytics/plugin.js
@@ -2,16 +2,12 @@ import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
 export default (ctx) => {
-  let enableAnalytics = false
   if (process.env.NUXT_ENV_GOOGLE_ANALYTICS_ID) {
-    enableAnalytics = true
-  }
-
-  if (enableAnalytics) {
     Vue.use(VueGtag, {
       config: {
         id: process.env.NUXT_ENV_GOOGLE_ANALYTICS_ID
       },
+      appName: 'mikesglitch',
       pageTrackerScreenviewEnabled: true,
       enabled: true
     }, ctx.app.router)
