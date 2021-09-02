c08799104b7484a25ea4f15030eb01ded2ba22e5
Michael Clark <mclarkgb@gmail.com>
2021-09-02 17:18:49 +0100

    updating ping code to handle failures

--- utils/PingBlog/Program.cs
+++ utils/PingBlog/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace PingBlog
 {
@@ -8,14 +7,22 @@ namespace PingBlog
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting ping blog service.  This service just keeps the blog alive even when nobody is visiting");
+            var client = new HttpClient();
             var startTimeSpan = TimeSpan.Zero;
             var runEveryXMinutes = TimeSpan.FromMinutes(2);
-
+            
             var timer = new System.Threading.Timer(async (e) =>
             {
-                var client = new HttpClient();
-                var response = await client.GetAsync("https://mikesglitch.com");
-                Console.WriteLine($"Pinged blog and got response status code: {response.StatusCode}");
+                try
+                {
+                    var response = await client.GetAsync("https://mikesglitch.com");
+                    Console.WriteLine($"Pinged blog and got response status code: {response.StatusCode}");
+                } 
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Could not ping blog.  Error: {ex}");
+                }
             }, null, startTimeSpan, runEveryXMinutes);
 
             Console.ReadLine();
