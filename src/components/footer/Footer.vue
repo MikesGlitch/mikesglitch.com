92eebd6c2b81af56abdf7a85fe61092781a5087b
Michael Clark <mclarkgb@gmail.com>
2023-06-24 12:08:16 +0100

    mobile bits

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -3,15 +3,19 @@
     <div class="container">
       <hr class="mb-4 text-gray-medium-2 dark:text-gray-light">
       <div class="flex flex-col gap-2.5 w-full h-full">
-        <div class="flex gap-2 align-middle items-center">
-          <span class="w-8 text-xl leading-4">©</span>
-          <span><span class="font-bold">Made by</span> <LinkExternal title="Message me" href="https://www.linkedin.com/in/mikesglitch" text="Michael Clark" /> 2020 – present</span>
+        <div class="flex flex-col sm:flex-row gap-2 align-middle items-center">
+          <span class="flex gap-2">
+            <span class="hidden sm:block w-8 text-xl leading-4">©</span>
+            <span><span class="font-bold">Made by</span> <LinkExternal title="Message me" href="https://www.linkedin.com/in/mikesglitch" text="Michael Clark" /></span>
+          </span>
+          2020 – present
         </div>
-        <div class="flex gap-2 align-middle items-center">
-          <span class="w-8">🔨</span><span class="font-bold">Built with</span> <link-external title="Vue" href="https://vuejs.org/" text="Vue" /> & <link-external title="Nuxt" href="https://v3.nuxtjs.org/" text="Nuxt" /> on <link-external title="Vercel" href="https://vercel.com/" text="Vercel" />
+        <div class="flex flex-col sm:flex-row gap-2 align-middle items-center">
+          <span class="flex gap-2"><span class="hidden sm:block w-8">🔨</span><span class="font-bold">Built with</span></span> <span><link-external title="Vue" href="https://vuejs.org/" text="Vue" /> & <link-external title="Nuxt" href="https://v3.nuxtjs.org/" text="Nuxt" /> on <link-external title="Vercel" href="https://vercel.com/" text="Vercel" /></span>
         </div>
-        <div v-if="data?.lastDeployment" class="flex gap-2 align-middle items-center">
-          <span class="w-8">🚀</span> <span class="font-bold">Last build</span> <link-external :title="toHumanReadableDate(data.lastDeployment.lastDeployedAt)" :href="`https://github.com/MikesGlitch/mikesglitch.com/commit/${data.lastDeployment.commitSha}`" :text="toHumanReadableDate(data.lastDeployment.lastDeployedAt)" />
+        <div v-if="data?.lastDeployment" class="flex flex-col sm:flex-row gap-2 align-middle items-center">
+          <span class="flex gap-2"><span class="hidden sm:block w-8">🚀</span> <span class="font-bold">Last build</span></span>
+          <link-external :title="toHumanReadableDate(data.lastDeployment.lastDeployedAt)" :href="`https://github.com/MikesGlitch/mikesglitch.com/commit/${data.lastDeployment.commitSha}`" :text="toHumanReadableDate(data.lastDeployment.lastDeployedAt)" />
         </div>
       </div>
     </div>
