8d30302cf60219641607488a73bacbcb96a357a5
Mike Clark <mclarkgb@gmail.com>
2023-08-19 21:38:59 +0100

    making text white on dark theme instead of grey for prose

--- src/components/text/Prose.vue
+++ src/components/text/Prose.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="break-words prose dark:prose-invert max-w-none prose-a:no-underline hover:prose-a:underline hover:prose-a:text-hotpink prose-a:text-hotpink prose-a:underline-offset-4">
+  <div class="break-words prose dark:prose-invert dark:text-white max-w-none prose-a:no-underline hover:prose-a:underline hover:prose-a:text-hotpink prose-a:text-hotpink prose-a:underline-offset-4">
     <slot />
   </div>
 </template>
